e4372703de2529b7c6c4e5195211f85654cb335b
Yannik Sinthern <yannik.sinthern@eecc.de>
2024-11-05 11:42:57 +0100

    color const names

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,7 +5,7 @@ class AppColors {
   static const Color secondary = Color.fromARGB(255, 233, 224, 200);
   static const Color borderColor = Color.fromARGB(255, 122, 122, 122);
 
-  static const Color white = Colors.white;
-  static const Color black = Colors.black;
-  static const Color grey = Colors.grey;
+  static const Color lightText = Colors.white;
+  static const Color darkText = Colors.black;
+  static const Color shadowColor = Colors.grey;
 }
